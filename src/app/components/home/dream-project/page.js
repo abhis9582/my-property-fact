0b0c99728982a4e0e2a6fd337fe2db671e551403
Abhishek Kumar <abhishek8601679@gmail.com>
2025-02-20 11:35:39 +0530

    fixed issue

--- src/app/components/home/dream-project/page.js
+++ src/app/components/home/dream-project/page.js
@@ -5,61 +5,54 @@ export default function DreamProject() {
         <p className="h3 text-center fw-bold">
           Find your dream property in the city you are searching in
         </p>
-        <div className="d-flex  justify-content-center flex-wrap">
-          <div className="ms-3 my-2">
+        <div className="container flex-lg-nowrap d-flex flex-wrap gap-3 justify-content-center">
+          <div className="my-2">
             <img
-              className="insight-image"
               src="/dream-cities/mumbai1.webp"
               alt="img-nri"
             />
           </div>
-          <div className="ms-3 my-2">
+          <div className="my-2">
             <img
-              className="insight-image"
               src="/dream-cities/pune1.webp"
               alt="img-nri"
             />
           </div>
-          <div className="ms-3 my-2">
+          <div className="my-2">
             <img
-              className="insight-image"
               src="/dream-cities/bangalore1.webp"
               alt="img-nri"
             />
           </div>
-          <div className="ms-3 my-2">
+          <div className="my-2">
             <img
-              className="insight-image"
               src="/dream-cities/delhi1.webp"
               alt="img-nri"
             />
           </div>
         </div>
-        <div className="d-flex justify-content-center flex-wrap">
+        <div className="container flex-lg-nowrap d-flex flex-wrap gap-3 justify-content-center">
           <div className="ms-3 my-2">
             <img
-              className="insight-image"
               src="/dream-cities/noida1.webp"
               alt="img-nri"
             />
           </div>
           <div className="ms-3 my-2">
             <img
-              className="insight-image"
               src="/dream-cities/gurugram1.webp"
               alt="img-nri"
             />
           </div>
           <div className="ms-3 my-2">
             <img
-              className="insight-image"
-              src="/dream-cities/2024-10-22_15-08-44_grnoida.jpg"
+              src="/dream-cities/gurugram1.webp"
               alt="img-nri"
+              className="w-100"
             />
           </div>
           <div className="ms-3 my-2">
             <img
-              className="insight-image"
               src="/dream-cities/other-cities.webp"
               alt="img-nri"
             />
