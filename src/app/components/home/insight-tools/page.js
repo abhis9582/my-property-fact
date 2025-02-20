c476d4afce7995073940caa627199230bfdb29cb
Abhishek Kumar <abhishek8601679@gmail.com>
2025-02-20 11:39:50 +0530

    fixed bug

--- src/app/components/home/insight-tools/page.js
+++ src/app/components/home/insight-tools/page.js
@@ -1,5 +1,4 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./insight.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
