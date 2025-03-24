5da36ee6bc52f39e7029986fe07d3f908f7c4340
Abhishek Kumar <abhishek8601679@gmail.com>
2025-03-24 17:13:47 +0530

    updated config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "http",
+  //       hostname: "localhost",
+  //       port: "8005"
+  //     },
+  //   ],
+  // },
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8005"
+        protocol: "https",
+        hostname: "api.mypropertyfact.com",
+        port: "8080"
       },
     ],
   },
