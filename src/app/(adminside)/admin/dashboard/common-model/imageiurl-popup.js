178ef7bb825db6b259bbd8e686b108a40a87518e
Abhishek Kumar <abhishek8601679@gmail.com>
2025-05-09 12:30:36 +0530

    bug

--- src/app/(adminside)/admin/dashboard/common-model/imageiurl-popup.js
+++ src/app/(adminside)/admin/dashboard/common-model/imageiurl-popup.js
@@ -1,10 +1,13 @@
 "use client";
+// import { LoadingSpinner } from "@/app/(home)/contact-us/page";
 import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Paper } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+// import { Paper } from "@mui/material";
+// import { DataGrid } from "@mui/x-data-grid";
+import Image from "next/image";
 import { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+// import { Button, Form, Modal } from "react-bootstrap";
+// import CommonModal from "./common-model";
 
 export default function ImageUrlPopup({ confirmBox, setConfirmBox }) {
     const [showModal, setShowModal] = useState(false);
@@ -93,84 +96,85 @@ export default function ImageUrlPopup({ confirmBox, setConfirmBox }) {
 
     const paginationModel = { page: 0, pageSize: 10 };
     return (
-        <Modal show={confirmBox} onHide={() => setConfirmBox(false)} centered>
-            <Modal.Header closeButton>
-                <Modal.Title>Copy image url to add in blog</Modal.Title>
-            </Modal.Header>
-            <div>
-                <div className="d-flex justify-content-between mt-3">
-                    <p className="h1 ">{pageObject.pageHeading}</p>
-                    <Button className="mx-3 btn btn-success" onClick={() => openAddModel()}>
-                        {pageObject.headingbuttonName}
-                    </Button>
-                </div>
-                <div className="table-container mt-5">
-                    <Paper sx={{ height: 550, width: "100%" }}>
-                        <DataGrid
-                            rows={allHeadersList}
-                            columns={columns}
-                            initialState={{ pagination: { paginationModel } }}
-                            pageSizeOptions={[10, 15, 20, 50]}
-                            checkboxSelection
-                            sx={{
-                                border: 0,
-                                "& .MuiDataGrid-columnHeader": {
-                                    fontWeight: "bold", // Make headings bold
-                                    fontSize: "16px", // Optional: Adjust size
-                                    backgroundColor: "#68ac78", // Optional: Light background
-                                },
-                            }}
-                        />
-                    </Paper>
-                </div>
-                {/* Modal for adding a new header */}
-                <Modal show={showModal} onHide={() => setShowModal(false)} centered>
-                    <Modal.Header closeButton>
-                        <Modal.Title>{title}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                            <Form.Group className="mb-3" controlId="headerName">
-                                <Form.Label>Category Display Name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter category display name"
-                                    name="categoryDisplayName"
-                                    onChange={(e) => handleChange(e)}
-                                    value={formData.categoryDisplayName || ""}
-                                    required
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    Category Display name is required
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="subHeaderName">
-                                <Form.Label>Category</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter category"
-                                    name="category"
-                                    onChange={(e) => handleChange(e)}
-                                    value={formData.category || ""}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    Category is required
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Button className="btn btn-success" type="submit" disabled={showLoading}>
-                                {buttonName} <LoadingSpinner show={showLoading} />
-                            </Button>
-                        </Form>
-                    </Modal.Body>
-                </Modal>
-                {/* Pass the necessary props to CommonModal */}
-                <CommonModal
-                    confirmBox={confirmBox}
-                    setConfirmBox={setConfirmBox}
-                    api={api}
-                    fetchAllHeadersList={fetchAllHeadersList}
-                />
-            </div>
-        </Modal>
+        <></>
+        // <Modal show={confirmBox} onHide={() => setConfirmBox(false)} centered>
+        //     <Modal.Header closeButton>
+        //         <Modal.Title>Copy image url to add in blog</Modal.Title>
+        //     </Modal.Header>
+        //     <div>
+        //         <div className="d-flex justify-content-between mt-3">
+        //             <p className="h1 ">{pageObject.pageHeading}</p>
+        //             <Button className="mx-3 btn btn-success" onClick={() => openAddModel()}>
+        //                 {pageObject.headingbuttonName}
+        //             </Button>
+        //         </div>
+        //         <div className="table-container mt-5">
+        //             <Paper sx={{ height: 550, width: "100%" }}>
+        //                 <DataGrid
+        //                     rows={allHeadersList}
+        //                     columns={columns}
+        //                     initialState={{ pagination: { paginationModel } }}
+        //                     pageSizeOptions={[10, 15, 20, 50]}
+        //                     checkboxSelection
+        //                     sx={{
+        //                         border: 0,
+        //                         "& .MuiDataGrid-columnHeader": {
+        //                             fontWeight: "bold", // Make headings bold
+        //                             fontSize: "16px", // Optional: Adjust size
+        //                             backgroundColor: "#68ac78", // Optional: Light background
+        //                         },
+        //                     }}
+        //                 />
+        //             </Paper>
+        //         </div>
+        //         {/* Modal for adding a new header */}
+        //         <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+        //             <Modal.Header closeButton>
+        //                 <Modal.Title>{title}</Modal.Title>
+        //             </Modal.Header>
+        //             <Modal.Body>
+        //                 <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        //                     <Form.Group className="mb-3" controlId="headerName">
+        //                         <Form.Label>Category Display Name</Form.Label>
+        //                         <Form.Control
+        //                             type="text"
+        //                             placeholder="Enter category display name"
+        //                             name="categoryDisplayName"
+        //                             onChange={(e) => handleChange(e)}
+        //                             value={formData.categoryDisplayName || ""}
+        //                             required
+        //                         />
+        //                         <Form.Control.Feedback type="invalid">
+        //                             Category Display name is required
+        //                         </Form.Control.Feedback>
+        //                     </Form.Group>
+        //                     <Form.Group className="mb-3" controlId="subHeaderName">
+        //                         <Form.Label>Category</Form.Label>
+        //                         <Form.Control
+        //                             type="text"
+        //                             placeholder="Enter category"
+        //                             name="category"
+        //                             onChange={(e) => handleChange(e)}
+        //                             value={formData.category || ""}
+        //                         />
+        //                         <Form.Control.Feedback type="invalid">
+        //                             Category is required
+        //                         </Form.Control.Feedback>
+        //                     </Form.Group>
+        //                     <Button className="btn btn-success" type="submit" disabled={showLoading}>
+        //                         {buttonName} <LoadingSpinner show={showLoading} />
+        //                     </Button>
+        //                 </Form>
+        //             </Modal.Body>
+        //         </Modal>
+        //         {/* Pass the necessary props to CommonModal */}
+        //         <CommonModal
+        //             confirmBox={confirmBox}
+        //             setConfirmBox={setConfirmBox}
+        //             api={api}
+        //             fetchAllHeadersList={fetchAllHeadersList}
+        //         />
+        //     </div>
+        // </Modal>
     )
 }
