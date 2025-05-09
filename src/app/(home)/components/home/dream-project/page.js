90a88811a87992c183355a597143eb70fc2ca39c
Abhishek Kumar <abhishek8601679@gmail.com>
2025-05-09 15:14:07 +0530

    changed city images on the home page

--- src/app/(home)/components/home/dream-project/page.js
+++ src/app/(home)/components/home/dream-project/page.js
@@ -1,6 +1,7 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-
+import "./dreamcity.css";
 export default function DreamProject() {
 
   //Cities json data
@@ -8,49 +9,49 @@ export default function DreamProject() {
     {
       name: "Agra",
       link: "/city/agra",
-      image: "/dream-cities/agranew.jpg",
+      image: "/dream-cities/agra.jpg",
       alt: "city-agra"
     },
     {
       name: "Bangalore",
       link: "/city/bangalore",
-      image: "/dream-cities/bangalorenew.jpg",
+      image: "/dream-cities/bangalore.jpg",
       alt: "city-bangalore"
     },
     {
       name: "Noida",
       link: "/city/noida",
-      image: "/dream-cities/noidanew.jpg",
+      image: "/dream-cities/noida.jpg",
       alt: "city-noida"
     },
     {
       name: "Delhi",
       link: "/city/delhi",
-      image: "/dream-cities/delhinew.jpg",
+      image: "/dream-cities/delhi.jpg",
       alt: "city-delhi"
     },
     {
       name: "Ghaziabad",
       link: "/city/ghaziabad",
-      image: "/dream-cities/ghaziabadnew.jpg",
+      image: "/dream-cities/ghaziabad.jpg",
       alt: "city-ghaziabad"
     },
     {
       name: "Jaipur",
       link: "/city/jaipur",
-      image: "/dream-cities/jaipurnew.jpg",
+      image: "/dream-cities/jaipur.jpg",
       alt: "city-jaipur"
     },
     {
       name: "Mumbai",
       link: "/city/mumbai",
-      image: "/dream-cities/mumbainew.jpg",
+      image: "/dream-cities/mumbai.jpg",
       alt: "city-mumbai"
     },
     {
       name: "Gurugram",
       link: "/city/gurugram",
-      image: "/dream-cities/gurugramnew.jpg",
+      image: "/dream-cities/gurugram.jpg",
       alt: "city-gurugram"
     }
   ];
@@ -60,15 +61,32 @@ export default function DreamProject() {
         <div className="row justify-content-center">
           {cities.map((city, index) => (
             <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 my-3 d-flex justify-content-center">
-              <Link href={city.link}>
-                <Image
-                  src={city.image}
-                  alt={city.alt}
-                  width={300}
-                  height={180}
-                  className="img-fluid rounded shadow-sm"
-                />
-              </Link>
+              <div className="city-flip-container">
+                <div className="city-flip-inner">
+                  <div className="city-flip-front">
+                    <Link href={city.link}>
+                      <Image
+                        src={city.image}
+                        alt={city.alt}
+                        width={300}
+                        height={180}
+                        className="img-fluid rounded shadow-sm"
+                      />
+                    </Link>
+                  </div>
+                  <div className="city-flip-back">
+                    <Link href={city.link}>
+                      <Image
+                        src={city.image} // Add backImage to your city object
+                        alt={`Back of ${city.alt}`}
+                        width={300}
+                        height={180}
+                        className="img-fluid rounded shadow-sm"
+                      />
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
