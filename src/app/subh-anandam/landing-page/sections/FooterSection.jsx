578f50aa258848f6bd3adea4aa21242e002f8dc1
Dhiraj <dkskp2005@gmail.com>
2025-06-17 14:52:17 +0530

    links removed

--- src/app/subh-anandam/landing-page/sections/FooterSection.jsx
+++ src/app/subh-anandam/landing-page/sections/FooterSection.jsx
@@ -1,64 +1,48 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import logo from '../assets/logo.png';
-import facebook from '../assets/Facebook.png';
-import twitter from '../assets/Twitter.png';
-import youtube from '../assets/Youtube.png';
-import instagram from '../assets/Instagram.png';
-import pinterest from '../assets/Pinterest.png';
+import React from "react";
+import Image from "next/image";
+import logo from "../assets/logo.png";
+import facebook from "../assets/Facebook.png";
+import twitter from "../assets/Twitter.png";
+import youtube from "../assets/Youtube.png";
+import instagram from "../assets/Instagram.png";
+import pinterest from "../assets/Pinterest.png";
 
 const FooterSection = () => {
-    const socialLinks = [
-        { src: facebook, alt: 'Facebook', href: '#' },
-        { src: twitter, alt: 'Twitter', href: '#' },
-        { src: youtube, alt: 'YouTube', href: '#' },
-        { src: instagram, alt: 'Instagram', href: '#' },
-        { src: pinterest, alt: 'Pinterest', href: '#' },
-    ];
+  return (
+    <footer
+      className="footer-section pt-5"
+      style={{ backgroundColor: "#1d1e22" }}
+    >
+      {/* Logo on Top Center */}
+      <div className="text-center mb-4">
+        <Image src={logo} alt="Logo" width={120} height={80} />
+      </div>
 
-    return (
-        <footer className="footer-section pt-5" style={{ backgroundColor: '#1d1e22' }}>
-            {/* Logo on Top Center */}
-            <div className="text-center mb-4">
-                <Image src={logo} alt="Logo" width={120} height={80} />
-            </div>
+      {/* Keywords */}
+      <div className="keywords d-flex justify-content-center align-items-center flex-wrap gap-3 py-3 px-3">
+        {[
+          "Trust",
+          "Community",
+          "Sustainability",
+          "Legacy",
+          "Comfort",
+          "Vision",
+        ].map((word, idx) => (
+          <React.Fragment key={idx}>
+            {idx > 0 && <span className="divider">|</span>}
+            <span className="keyword">{word}</span>
+          </React.Fragment>
+        ))}
+      </div>
 
-            {/* White Background: Follow Us + Social Icons */}
-            <div className="white-section py-3 px-4 mb-3 mx-auto">
-                <div className="d-flex align-items-center justify-content-center gap-3 flex-wrap">
-                    <p className="follow-text mb-0">Follow us:</p>
-                    {socialLinks.map(({ src, alt, href }, index) => (
-                        <a
-                            key={index}
-                            href={href}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="icon-link"
-                        >
-                            <Image src={src} alt={alt} width={32} height={32} />
-                        </a>
-                    ))}
-                </div>
-            </div>
+      {/* Copyright */}
+      <div className="black-section text-white text-center py-3 px-2">
+        © {new Date().getFullYear()} Shubh Anandam. All rights reserved.
+      </div>
 
-            {/* Keywords */}
-            <div className="keywords d-flex justify-content-center align-items-center flex-wrap gap-3 py-3 px-3">
-                {['Trust', 'Community', 'Sustainability', 'Legacy', 'Comfort', 'Vision'].map((word, idx) => (
-                    <React.Fragment key={idx}>
-                        {idx > 0 && <span className="divider">|</span>}
-                        <span className="keyword">{word}</span>
-                    </React.Fragment>
-                ))}
-            </div>
-
-            {/* Copyright */}
-            <div className="black-section text-white text-center py-3 px-2">
-                © {new Date().getFullYear()} Shubh Anandam. All rights reserved.
-            </div>
-
-            <style jsx>{`
+      <style jsx>{`
         .white-section {
           background-color: #ffffff;
           width: fit-content;
@@ -121,8 +105,8 @@ const FooterSection = () => {
           }
         }
       `}</style>
-        </footer>
-    );
+    </footer>
+  );
 };
 
 export default FooterSection;
