25406bc8fad55955e8785582d2fa3679b081bf61
Dhiraj <dkskp2005@gmail.com>
2025-06-18 13:02:42 +0530

    header and cta

--- src/app/subh-anandam/landing-page/page.js
+++ src/app/subh-anandam/landing-page/page.js
@@ -2,14 +2,12 @@ import React from "react";
 import HeroSection from "./sections/HeroSection";
 import MainSection from "./sections/MainSection";
 import FooterSection from "./sections/FooterSection";
-import Header from "./sections/Header";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const page = () => {
   return (
     <>
-      <Header />
       <HeroSection />
       <MainSection />
       <FooterSection />
