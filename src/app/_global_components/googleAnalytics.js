b5f2c8a279224cee5bf188f832cec309825f43b6
Abhishek kumar <abhishek8601679@gmail.com>
2025-06-23 13:07:42 +0530

    added new folder for global components

---
+++ src/app/_global_components/googleAnalytics.js
@@ -0,0 +1,28 @@
+// components/GoogleAnalytics.js
+'use client';
+
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+    const GA_MEASUREMENT_ID = 'G-4F8FTBNL24';
+
+    return (
+        <>
+            {/* Load gtag.js script */}
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                strategy="afterInteractive"
+            />
+
+            {/* Initialize GA */}
+            <Script id="ga-init" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+            </Script>
+        </>
+    );
+}
