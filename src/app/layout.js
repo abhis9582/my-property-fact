ff9814f7e68096ae0f03d82439f02c7db5c4cf85
Abhishek kumar <abhishek8601679@gmail.com>
2025-06-23 13:07:14 +0530

    added google analytics code for .in domain

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Script from 'next/script';
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import GoogleAnalytics from "./_global_components/googleAnalytics";
 // app/layout.js
 export const metadata = {
     title: "Not found",
@@ -33,50 +34,9 @@ export default function RootLayout({ children }) {
                 </Script>
             </head>
             <body>
+                <GoogleAnalytics />
                 <main>{children}</main>
                 <ToastContainer />
-                {/* ✅ Google Tag Manager */}
-                <Script
-                    id="gtm-script"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XYZ');
-            `,
-                    }}
-                />
-
-                {/* ✅ Google Analytics */}
-                <Script
-                    src={`https://www.googletagmanager.com/gtag/js?id=G-QCFJENDW3M`}
-                    strategy="afterInteractive"
-                />
-                <Script
-                    id="ga-init"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-QCFJENDW3M');
-            `,
-                    }}
-                />
-                {/* Noscript fallback for Facebook Pixel */}
-                <noscript>
-                    <img
-                        height="1"
-                        width="1"
-                        style={{ display: 'none' }}
-                        src="https://www.facebook.com/tr?id=1153798529848547&ev=PageView&noscript=1"
-                        alt=""
-                    />
-                </noscript>
             </body>
         </html>
     );
