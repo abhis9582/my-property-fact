d5eb78b681fbcdfd7467407fadfff0abab8a2445
= <=>
2025-09-19 09:58:04 +0530

    Updated

--- src/app/(home)/components/home/featured/page.js
+++ src/app/(home)/components/home/featured/page.js
@@ -1,21 +1,43 @@
-import { fetchAllProjects, getAllProjects } from "@/app/_global_components/masterFunction";
+import {
+  fetchAllProjects,
+  getAllProjects,
+} from "@/app/_global_components/masterFunction";
 import Featured from "./featured";
 
 export default async function FeaturedPage({
   url,
   autoPlay,
-  projectsList = [],
-  category
+  allFeaturedProperties = [],
+  category,
+  type,
 }) {
   const projects = await getAllProjects();
-  console.log(projects.filter(item=> item.propertyTypeName === category).length, "featured projects length");
-  
+  console.log(
+    projects.filter((item) => item.propertyTypeName === category).length,
+    "featured projects length"
+  );
+  if (allFeaturedProperties.length > 0) {
+    return (
+      <Featured
+        url={url}
+        allProjects={allFeaturedProperties}
+        autoPlay={autoPlay}
+        type={type}
+      />
+    );
+  }
   return (
     <>
       <Featured
         // allFeaturedProperties={projects}
         url={url}
-        allProjects={category ? projects.filter(item=> item.propertyTypeName === category).slice(0, 20) : projects.slice(0, 3)} // Display only first 6 projects
+        allProjects={
+          category
+            ? projects
+                .filter((item) => item.propertyTypeName === category)
+                .slice(0, 20)
+            : projects.slice(0, 3)
+        } // Display only first 6 projects
         autoPlay={autoPlay}
       />
     </>
