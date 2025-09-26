261fd44000ec9cfb618ae0959714a4306decc34d
= <=>
2025-09-26 13:58:11 +0530

    update

--- src/app/landing-pages/eldeco-wow/[path]/layout.js
+++ src/app/landing-pages/eldeco-wow/[path]/layout.js
@@ -8,8 +8,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+      <main>{children}</main>
   );
 }
---
+++ src/app/landing-pages/eldeco-wow/layout.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../globals.css";
+
+export const metadata = {
+  title: "Edeco Wow",
+  description: "Edeco Wow Landing Page",
+};
+
+export default function RootLayout({ children }) {
+  return <main>{children}</main>;
+}
