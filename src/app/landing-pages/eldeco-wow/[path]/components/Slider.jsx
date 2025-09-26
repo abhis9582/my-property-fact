2478a05b78c474f96bc79c283ae3ffca61d9aa5b
SyedArhanRizvi <arhanrizvi9@gmail.com>
2025-09-26 16:13:20 +0530

    Update Slideer And Some Responsiveness

--- src/app/landing-pages/eldeco-wow/[path]/components/Slider.jsx
+++ src/app/landing-pages/eldeco-wow/[path]/components/Slider.jsx
@@ -1,6 +1,5 @@
 "use client";
-import { useState } from "react";
-import { useSlider } from "../hooks/useSlider";
+import { useState, useEffect, useRef } from "react";
 
 const sliderCards = [
   {
@@ -31,22 +30,44 @@ const sliderCards = [
 ];
 
 export default function Slider() {
-  const { sliderRef, next, prev } = useSlider();
+  const sliderRef = useRef(null);
+  // 👇 default activeIndex = 1 (means second card center me show hoga)
   const [activeIndex, setActiveIndex] = useState(1);
 
-  // Update active index for pagination dots
+  // Scroll to active card and keep it centered
+useEffect(() => {
+  if (sliderRef.current) {
+    const card = sliderRef.current.children[activeIndex];
+    if (card) {
+      sliderRef.current.scrollTo({
+        left: card.offsetLeft - sliderRef.current.offsetWidth / 2 + card.offsetWidth / 2,
+        behavior: "smooth",
+      });
+    }
+  }
+}, [activeIndex]);
+
+
+  // Auto slide every 3 sec
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prev) =>
+        prev === sliderCards.length - 1 ? 0 : prev + 1
+      );
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   const handleNext = () => {
-    setActiveIndex((prevIndex) =>
-      prevIndex === sliderCards.length - 1 ? 0 : prevIndex + 1
+    setActiveIndex((prev) =>
+      prev === sliderCards.length - 1 ? 0 : prev + 1
     );
-    next();
   };
 
   const handlePrev = () => {
-    setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? sliderCards.length - 1 : prevIndex - 1
+    setActiveIndex((prev) =>
+      prev === 0 ? sliderCards.length - 1 : prev - 1
     );
-    prev();
   };
 
   return (
@@ -54,30 +75,26 @@ export default function Slider() {
       {/* Slider Track */}
       <div
         ref={sliderRef}
-        className={`d-flex overflow-x-auto overflow-md-hidden scroll-smooth gap-3 elementclass`}
-        style={{
-          scrollBehavior: "smooth",
-        }}
+        className="d-flex overflow-x-auto scroll-smooth gap-3 elementclass"
+        style={{ scrollBehavior: "smooth" }}
       >
-        {sliderCards.map((card, index) => (
+        {sliderCards.map((card) => (
           <div
             key={card.id}
-            className={`slider-card d-flex flex-column align-items-center bg-white text-black h-auto h-md-478 flex-shrink-0`}
+            className="slider-card d-flex flex-column align-items-center bg-white text-black h-auto h-lg-478 flex-shrink-0"
             style={{
               width: "598px",
               maxWidth: "100%",
+              scrollSnapAlign: "center",
             }}
           >
             <img
               src={card.image}
-              className="w-100 h-auto h-md-408"
+              className="w-100 h-auto h-lg-408"
               alt=""
               style={{ objectFit: "cover" }}
             />
-            <div
-              className="w-100 text-center p-2"
-              style={{ maxWidth: "428px" }}
-            >
+            <div className="w-100 text-center p-2" style={{ maxWidth: "428px" }}>
               <p
                 style={{
                   fontFamily: "moster_regular",
@@ -92,64 +109,32 @@ export default function Slider() {
         ))}
       </div>
 
-      {/* Navigation buttons (absolute on mobile, centered below on desktop) */}
+      {/* Navigation buttons */}
       <div>
-        {/* Desktop: normal below */}
-        <div className="d-none d-md-flex mt-4 justify-content-center align-items-center gap-3">
-          <button
-            onClick={handlePrev}
-            className="slider-nav-button border-0"
-            style={{
-              backgroundColor: "white",
-              borderRadius: "50%",
-            }}
-          >
-          
- <svg
-              width="32"
-              height="24"
-              viewBox="0 0 32 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0.939522 10.9393C0.353735 11.5251 0.353735 12.4749 0.939522 13.0607L10.4855 22.6066C11.0712 23.1924 12.021 23.1924 12.6068 22.6066C13.1926 22.0208 13.1926 21.0711 12.6068 20.4853L4.1215 12L12.6068 3.51472C13.1926 2.92893 13.1926 1.97919 12.6068 1.3934C12.021 0.807611 11.0712 0.807611 10.4855 1.3934L0.939522 10.9393ZM32.0002 12V10.5L2.00018 10.5V12V13.5L32.0002 13.5V12Z"
-                fill="black"
-              />
-            </svg>
+        <div className="d-none d-lg-flex mt-4 justify-content-center align-items-center gap-3">
+          <button onClick={handlePrev} className="slider-nav-button border-0" style={{ backgroundColor: "white", borderRadius: "50%" }}>
+            
+<svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.939522 10.9393C0.353735 11.5251 0.353735 12.4749 0.939522 13.0607L10.4855 22.6066C11.0712 23.1924 12.021 23.1924 12.6068 22.6066C13.1926 22.0208 13.1926 21.0711 12.6068 20.4853L4.1215 12L12.6068 3.51472C13.1926 2.92893 13.1926 1.97919 12.6068 1.3934C12.021 0.807611 11.0712 0.807611 10.4855 1.3934L0.939522 10.9393ZM32.0002 12V10.5L2.00018 10.5V12V13.5L32.0002 13.5V12Z" fill="black"/>
+</svg>
 
           </button>
-          <button
-            onClick={handleNext}
-            className="slider-nav-button border-0"
-            style={{
-              backgroundColor: "white",
-              borderRadius: "50%",
-            }}
-          >
-           <svg
-              width="32"
-              height="24"
-              viewBox="0 0 32 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M31.0605 10.9393C31.6463 11.5251 31.6463 12.4749 31.0605 13.0607L21.5145 22.6066C20.9288 23.1924 19.979 23.1924 19.3932 22.6066C18.8074 22.0208 18.8074 21.0711 19.3932 20.4853L27.8785 12L19.3932 3.51472C18.8074 2.92893 18.8074 1.97919 19.3932 1.3934C19.979 0.807611 20.9288 0.807611 21.5145 1.3934L31.0605 10.9393ZM-0.000183105 12L-0.000183105 10.5L29.9998 10.5V12V13.5L-0.000183105 13.5L-0.000183105 12Z"
-                fill="black"
-              />
-            </svg>
+          <button onClick={handleNext} className="slider-nav-button border-0" style={{ backgroundColor: "white", borderRadius: "50%" }}>
+            
+<svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M31.0605 10.9393C31.6463 11.5251 31.6463 12.4749 31.0605 13.0607L21.5145 22.6066C20.9288 23.1924 19.979 23.1924 19.3932 22.6066C18.8074 22.0208 18.8074 21.0711 19.3932 20.4853L27.8785 12L19.3932 3.51472C18.8074 2.92893 18.8074 1.97919 19.3932 1.3934C19.979 0.807611 20.9288 0.807611 21.5145 1.3934L31.0605 10.9393ZM-0.000183105 12L-0.000183105 10.5L29.9998 10.5V12V13.5L-0.000183105 13.5L-0.000183105 12Z" fill="black"/>
+</svg>
+
           </button>
         </div>
 
-        {/* Mobile: absolute overlay */}
-        <div className="d-flex d-md-none">
+        <div className="d-flex d-lg-none">
           <button
             onClick={handlePrev}
             className="border-0"
             style={{
               position: "absolute",
-              top: "50%",
+              top: "43%",
               left: "10px",
               transform: "translateY(-50%)",
               backgroundColor: "white",
@@ -157,25 +142,18 @@ export default function Slider() {
               padding: "8px",
             }}
           >
-            <svg
-              width="32"
-              height="24"
-              viewBox="0 0 32 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0.939522 10.9393C0.353735 11.5251 0.353735 12.4749 0.939522 13.0607L10.4855 22.6066C11.0712 23.1924 12.021 23.1924 12.6068 22.6066C13.1926 22.0208 13.1926 21.0711 12.6068 20.4853L4.1215 12L12.6068 3.51472C13.1926 2.92893 13.1926 1.97919 12.6068 1.3934C12.021 0.807611 11.0712 0.807611 10.4855 1.3934L0.939522 10.9393ZM32.0002 12V10.5L2.00018 10.5V12V13.5L32.0002 13.5V12Z"
-                fill="black"
-              />
-            </svg>
+            
+<svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.939522 10.9393C0.353735 11.5251 0.353735 12.4749 0.939522 13.0607L10.4855 22.6066C11.0712 23.1924 12.021 23.1924 12.6068 22.6066C13.1926 22.0208 13.1926 21.0711 12.6068 20.4853L4.1215 12L12.6068 3.51472C13.1926 2.92893 13.1926 1.97919 12.6068 1.3934C12.021 0.807611 11.0712 0.807611 10.4855 1.3934L0.939522 10.9393ZM32.0002 12V10.5L2.00018 10.5V12V13.5L32.0002 13.5V12Z" fill="black"/>
+</svg>
+
           </button>
           <button
             onClick={handleNext}
             className="border-0"
             style={{
               position: "absolute",
-              top: "50%",
+              top: "43%",
               right: "10px",
               transform: "translateY(-50%)",
               backgroundColor: "white",
@@ -183,24 +161,17 @@ export default function Slider() {
               padding: "8px",
             }}
           >
-            <svg
-              width="32"
-              height="24"
-              viewBox="0 0 32 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M31.0605 10.9393C31.6463 11.5251 31.6463 12.4749 31.0605 13.0607L21.5145 22.6066C20.9288 23.1924 19.979 23.1924 19.3932 22.6066C18.8074 22.0208 18.8074 21.0711 19.3932 20.4853L27.8785 12L19.3932 3.51472C18.8074 2.92893 18.8074 1.97919 19.3932 1.3934C19.979 0.807611 20.9288 0.807611 21.5145 1.3934L31.0605 10.9393ZM-0.000183105 12L-0.000183105 10.5L29.9998 10.5V12V13.5L-0.000183105 13.5L-0.000183105 12Z"
-                fill="black"
-              />
-            </svg>
+            
+<svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M31.0605 10.9393C31.6463 11.5251 31.6463 12.4749 31.0605 13.0607L21.5145 22.6066C20.9288 23.1924 19.979 23.1924 19.3932 22.6066C18.8074 22.0208 18.8074 21.0711 19.3932 20.4853L27.8785 12L19.3932 3.51472C18.8074 2.92893 18.8074 1.97919 19.3932 1.3934C19.979 0.807611 20.9288 0.807611 21.5145 1.3934L31.0605 10.9393ZM-0.000183105 12L-0.000183105 10.5L29.9998 10.5V12V13.5L-0.000183105 13.5L-0.000183105 12Z" fill="black"/>
+</svg>
+
           </button>
         </div>
       </div>
 
-      {/* Pagination dots (only mobile) */}
-      <div className="d-flex d-md-none justify-content-center mt-3 gap-2">
+      {/* Pagination dots (mobile) */}
+      <div className="d-flex d-lg-none justify-content-center mt-3 gap-2">
         {sliderCards.map((_, index) => (
           <div
             key={index}
