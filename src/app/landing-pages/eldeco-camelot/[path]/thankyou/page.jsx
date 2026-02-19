e5b74e5ba93fb091005668a8c815a20f66621a57
itzz-aryan-121 <aryan.22b0131042@abes.ac.in>
2026-02-19 10:27:16 +0530

    fix:script updated in eldeco landing page

--- src/app/landing-pages/eldeco-camelot/[path]/thankyou/page.jsx
+++ src/app/landing-pages/eldeco-camelot/[path]/thankyou/page.jsx
@@ -62,20 +62,15 @@ export default function ThankYouPage() {
         >
           {`
             window._tfa = window._tfa || [];
-            window._tfa.push({ notify: 'event', name: 'page_view', id: 1776057 });
-            (function(t, f, a, x) {
+            window._tfa.push({ notify: 'event', name: 'page_view', id: 1723008 });
+            !function (t, f, a, x) {
               if (!document.getElementById(x)) {
-                t.async = 1;
-                t.src = a;
-                t.id = x;
-                f.parentNode.insertBefore(t, f);
+                t.async = 1; t.src = a; t.id = x; f.parentNode.insertBefore(t, f);
               }
-            })(
-              document.createElement('script'),
-              document.getElementsByTagName('script')[0],
-              'https://cdn.taboola.com/libtrc/unip/1776057/tfa.js',
-              'tb_tfa_script'
-            );
+            }(document.createElement('script'),
+            document.getElementsByTagName('script')[0],
+            '//cdn.taboola.com/libtrc/unip/1723008/tfa.js',
+            'tb_tfa_script');
           `}
         </Script>
       )}
